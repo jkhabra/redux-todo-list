d38814ba6d8261e3a090e85b94d418034308a3ec
jatinder <jatinder1.singh@trantorinc.com>
2023-02-03 18:24:43 +0530

    feat: add basic scss styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Todo from "./todo";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
@@ -11,10 +11,6 @@ function App() {
       <PersistGate persistor={persistor}>
         <Provider store={store}>
           <div className="App">
-            <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-            </p>
-
             <Todo />
           </div>
         </Provider>
